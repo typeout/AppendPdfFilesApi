731aef58c42ebd044ed1f8b56faadd28b8c03b7e
typeout <kazius@gmail.com>
2023-06-24 14:34:23 -0500

    simple status controller

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FBR.API.Controllers
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = false)]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        ///<summary>Test API with OK request</summary>
+        ///<returns>Returns 200 OK response</returns>
+        ///<response code="200">Return OK</response>
+        ///<response code="400">Bad Request</response>
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("OK");
+    }
+}
