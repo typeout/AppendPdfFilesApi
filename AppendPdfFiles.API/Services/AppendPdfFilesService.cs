dc7bf87f8a2a62f00cd33a85b484b595181b4fc5
typeout <kazius@gmail.com>
2023-06-25 14:06:20 -0500

    append pdf files service implementation

---
+++ AppendPdfFiles.API/Services/AppendPdfFilesService.cs
@@ -0,0 +1,9 @@
+namespace AppendPdfFiles.API.Services;
+
+public class AppendPdfFilesService : IAppendPdfFilesService
+{
+    public Task<IFormFile> AppendPdfFilesAsync(IFormFileCollection pdfFiles)
+    {
+        throw new NotImplementedException();
+    }
+}
